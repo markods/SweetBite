894735373b6c68cab07a19b369197dec50b807cb
Marko Stanojević <marko.d.stanojevic@gmail.com>
2020-05-09 15:48:23 +0200

    dodata indeks stranica

---
+++ Slatki zalogaj/template.js
@@ -0,0 +1,12 @@
+$(document).ready(function () {
+    $("#sidebar").mCustomScrollbar({ theme: "minimal", });
+
+    $("#sidebar-collapse, #sidebar-dismiss").on('click', function () {
+        $("#sidebar").toggleClass("active");
+    });
+
+    let width = $(document).width();
+    if( width >= 768 )
+        $("#sidebar").addClass("active");
+});
+
